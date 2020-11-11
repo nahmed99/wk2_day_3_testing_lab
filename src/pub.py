7a83eae59f0d6818b610277fc2d6731197207b7b
Nawaz Ahmed <ahmed_nawaz@hotmail.com>
2020-11-11 12:59:06 +0000

    add money to till in pub class

--- src/pub.py
+++ src/pub.py
@@ -13,3 +13,26 @@ class Pub:
 
     def add_drink(self, drink):
         self.drinks.append(drink)
+
+
+    def find_drink_by_name(self, drink_name):
+        for drink in self.drinks:
+            if drink.name == drink_name:
+                return drink
+
+
+    def add_money_to_till(self, drink):
+        self.till += drink.price
+        print(" alsdjalsjdldjaldksjalsdkj",self.till)
+
+
+    def get_till_total(self):
+        return self.till
+
+
+
+    #def take_money_from_customer(self, customer, drink):
+    #    customer.wallet -= drink.price
+
+    def sell_drink_to_customer(self, drink_name):
+        pass
--- tests/pub_test.py
+++ tests/pub_test.py
@@ -5,6 +5,7 @@ from src.drink import Drink
 class TestPub(unittest.TestCase):
     def setUp(self):
         self.pub = Pub("The Red Lion", 50.0)
+        self.drink_1 = Drink("tea", 0.95)
         
     
     def test_pub_has_name(self):
@@ -25,3 +26,20 @@ class TestPub(unittest.TestCase):
         self.pub.add_drink(Drink("negroni", 7.95))
 
         self.assertEqual(3, self.pub.drinks_list_length())
+
+
+    def test_find_drink_by_name(self):
+        self.pub.add_drink(Drink("champagne", 30.0))
+        self.pub.add_drink(Drink("lemonade", 1.95))
+        self.pub.add_drink(Drink("negroni", 7.95))
+
+        drink_found = self.pub.find_drink_by_name("lemonade")
+        drink_not_found = self.pub.find_drink_by_name("hot chocolate")
+        self.assertEqual("lemonade", drink_found.name)
+        self.assertIsNone(drink_not_found)
+
+
+    def test_add_money_to_till(self):
+        self.pub.add_money_to_till(self.drink_1)
+        self.assertEqual(50.95, self.pub.get_till_total())
+
